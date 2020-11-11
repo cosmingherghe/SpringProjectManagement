de04ca8efe8e62eaa4395bf9ef2407df3fd762f8
Cosmin Gherghe <cosminlinux@gmail.com>
2020-11-11 23:53:33 +0000

    Reinventing the wheel

---
+++ src/test/java/dev/cosmingherghe/pma/ProjectManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cosmingherghe.pma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b1ad44c2ce82071f6b2057c1dc2b24e25ec1b8dd
Cosmin Gherghe <cosminlinux@gmail.com>
2020-11-12 01:27:11 +0000

    Update ProjectController to save Projects

--- src/main/java/dev/cosmingherghe/pma/controllers/ProjectController.java
+++ src/main/java/dev/cosmingherghe/pma/controllers/ProjectController.java
@@ -1,6 +1,8 @@
 package dev.cosmingherghe.pma.controllers;
 
+import dev.cosmingherghe.pma.dao.ProjectRepository;
 import dev.cosmingherghe.pma.entities.Project;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/projects")
 public class ProjectController {
 
+    @Autowired
+    ProjectRepository projectRepository;
+
     @GetMapping("/new")
     public String displayNewProjectForm(Model model) {
 
@@ -18,5 +23,12 @@ public class ProjectController {
 
         model.addAttribute("project", aProject);
         return "/new-project";
+    }
+
+    @PostMapping("/save")
+    public String saveProject(Project project, Model model) {
+        projectRepository.save(project);
+
+        return "redirect:/projects/new";  //used to prevent duplicate submissions
     }
 }

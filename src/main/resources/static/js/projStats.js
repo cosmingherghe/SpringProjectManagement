36092613e2fe0ff2c3573254f7a3776d9083b943
Cosmin Gherghe <cosminlinux@gmail.com>
2020-11-20 20:47:44 +0000

    Adding chartjs to project

--- src/main/resources/static/js/projStats.js
+++ src/main/resources/static/js/projStats.js
@@ -1,5 +1,17 @@
-var projStats = new Chart(ctx, {
+var projStats = new Chart(document.getElementById("myChart"), {
+    // The type of chart we want to create
     type: 'pie',
-    data: data,
-    options: options
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: ["#ff3000", "#5fd4ff", "#74fd38"],
+            borderColor: 'rgb(255,99,132)',
+            data: [2, 10, 5]
+        }]
+    },
+    // Configuration options go here
+    options: {}
 });

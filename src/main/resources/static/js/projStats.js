8b35ed32a34812b25bf7fcc648937972bced16f0
Cosmin Gherghe <cosminlinux@gmail.com>
2020-11-21 04:02:06 +0000

    fixing projects html table output

--- src/main/resources/static/js/projStats.js
+++ src/main/resources/static/js/projStats.js
@@ -1,17 +1,39 @@
-var projStats = new Chart(document.getElementById("projectsChart"), {
+var projStatsStr = decodeChartProjStageCount(chartProjStageCount);
+var chartJsonArray = JSON.parse(projStatsStr);
+var arrayLength = chartJsonArray.length;
+
+var numericData = [];
+var labelData = [];
+
+for(var i=0; i < arrayLength; i++) {
+    numericData[i] = chartJsonArray[i].total;
+    labelData[i] = chartJsonArray[i].stage;
+}
+
+new Chart(document.getElementById("projectsChart"), {
     // The type of chart we want to create
     type: 'pie',
 
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March'],
+        labels: labelData,
         datasets: [{
             label: 'My First dataset',
-            backgroundColor: ["#ff3000", "#5fd4ff", "#74fd38"],
-            borderColor: 'rgb(255,99,132)',
-            data: [2, 10, 5]
+            backgroundColor: ["#74fd38", "#5fd4ff", "#ff3000"],
+            data: numericData
         }]
     },
     // Configuration options go here
-    options: {}
+    options: {
+        title: {
+            display: true,
+            text: 'Project Statuses'
+        }
+    }
 });
+
+function decodeChartProjStageCount(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}

8e728e8ec98e3cda2257531778b9805473fa5484
Cosmin Gherghe <cosminlinux@gmail.com>
2020-11-26 23:56:04 +0000

    Add Conf for Integration Testing

--- src/main/java/dev/cosmingherghe/pma/dao/ProjectRepository.java
+++ src/main/java/dev/cosmingherghe/pma/dao/ProjectRepository.java
@@ -3,14 +3,18 @@ package dev.cosmingherghe.pma.dao;
 import dev.cosmingherghe.pma.dto.ChartProjStageCount;
 import dev.cosmingherghe.pma.entities.Project;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface ProjectRepository extends CrudRepository<Project, Long> {
+public interface ProjectRepository extends PagingAndSortingRepository<Project, Long> {
 
     @Query(nativeQuery = true, value = "SELECT stage, COUNT(*) AS total " +
                                         "FROM project " +
                                         "GROUP BY stage")
     public List<ChartProjStageCount> projStageCount();
+
+    @Override
+    public List<Project> findAll();
+
 }
---
+++ src/test/java/dev/cosmingherghe/dao/ProjectRepositoryIntegrationTest.java
@@ -0,0 +1,35 @@
+package dev.cosmingherghe.dao;
+
+import dev.cosmingherghe.pma.ProjectManagementAppApplication;
+import dev.cosmingherghe.pma.dao.ProjectRepository;
+import dev.cosmingherghe.pma.entities.Project;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
+
+@ContextConfiguration(classes = ProjectManagementAppApplication.class)
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@SqlGroup({@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql", "classpath:data.sql"}),
+                @Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:drop.sql")})
+public class ProjectRepositoryIntegrationTest {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @Test
+    public void ifNewProjectSaved_thenSuccess() {
+        Project newProject = new Project("New Test Project", "COMPLETE", "Test Description");
+        projectRepository.save(newProject);
+
+        assertEquals(5, projectRepository.findAll().size());
+    }
+}

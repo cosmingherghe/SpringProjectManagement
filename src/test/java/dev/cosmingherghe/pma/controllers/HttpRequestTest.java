d86a4affa65be488182be99645720444d545343f
Cosmin Gherghe <cosminlinux@gmail.com>
2020-11-27 01:22:13 +0000

    Add WebEnvironment Testing

---
+++ src/test/java/dev/cosmingherghe/pma/controllers/HttpRequestTest.java
@@ -0,0 +1,30 @@
+package dev.cosmingherghe.pma.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void homePageResturnsVersionNumberCorrectly_thenSuccess() {
+        String renderedHtml = this.testRestTemplate.getForObject("http://localhost:" + port + "/", String.class);
+        assertEquals(renderedHtml.contains("0.0.1"), true);
+    }
+
+}
--- src/test/java/dev/cosmingherghe/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/dev/cosmingherghe/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,4 +1,4 @@
-package dev.cosmingherghe.dao;
+package dev.cosmingherghe.pma.dao;
 
 import dev.cosmingherghe.pma.ProjectManagementAppApplication;
 import dev.cosmingherghe.pma.dao.ProjectRepository;
@@ -6,8 +6,7 @@ import dev.cosmingherghe.pma.entities.Project;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
@@ -15,21 +14,23 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
-@ContextConfiguration(classes = ProjectManagementAppApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@SqlGroup({@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql", "classpath:data.sql"}),
-                @Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:drop.sql")})
+@SqlGroup({
+        @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = {"classpath:schema.sql", "classpath:data.sql"}),
+        @Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:drop.sql")
+})
 public class ProjectRepositoryIntegrationTest {
 
     @Autowired
-    ProjectRepository projectRepository;
+    ProjectRepository proRepo;
 
     @Test
     public void ifNewProjectSaved_thenSuccess() {
         Project newProject = new Project("New Test Project", "COMPLETE", "Test Description");
-        projectRepository.save(newProject);
+        proRepo.save(newProject);
 
-        assertEquals(5, projectRepository.findAll().size());
+        assertEquals(5, proRepo.findAll().size());
+
     }
 }

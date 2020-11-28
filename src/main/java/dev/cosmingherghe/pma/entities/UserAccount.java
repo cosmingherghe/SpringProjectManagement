aaa9dca7f0133a54b753875798f6d383849c81b2
Cosmin Gherghe <cosminlinux@gmail.com>
2020-11-28 19:25:17 +0000

    fixing tables

--- src/main/java/dev/cosmingherghe/pma/entities/UserAccount.java
+++ src/main/java/dev/cosmingherghe/pma/entities/UserAccount.java
@@ -3,13 +3,11 @@ package dev.cosmingherghe.pma.entities;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user_accounts")
 public class UserAccount {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_accounts_generator")
-    @SequenceGenerator(name = "user_accounts_generator", sequenceName = "user_accounts_seq", allocationSize = 1)
-    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_account_generator")
+    @SequenceGenerator(name = "user_account_generator", sequenceName = "user_account_seq", allocationSize = 1)
     private Long userId;
 
     private String userName;

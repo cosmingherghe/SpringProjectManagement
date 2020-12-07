fd3c763e58dbbe10a438f0a711309af2f3bc790c
Cosmin Gherghe <cosminlinux@gmail.com>
2020-12-07 19:02:10 +0000

    Add PostMapping

--- src/main/java/dev/cosmingherghe/pma/api/controller/EmployeeApiController.java
+++ src/main/java/dev/cosmingherghe/pma/api/controller/EmployeeApiController.java
@@ -3,7 +3,10 @@ package dev.cosmingherghe.pma.api.controller;
 import dev.cosmingherghe.pma.entities.Employee;
 import dev.cosmingherghe.pma.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/app-api/employees")
@@ -21,5 +24,12 @@ public class EmployeeApiController {
     public Employee findByIdEmployee(@PathVariable("id") Long id) {
         return employeeService.findByIdApi(id);
     }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee addEmployee(@RequestBody @Valid Employee employee) {
+        return employeeService.save(employee);
+    }
+
 
 }

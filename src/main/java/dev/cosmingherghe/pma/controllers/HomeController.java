ffb1e71ea9146391ba9afb8d2eeb2387a181c777
Cosmin Gherghe <cosminlinux@gmail.com>
2020-12-07 00:13:39 +0000

    verify if there is a loggedInUser

--- src/main/java/dev/cosmingherghe/pma/controllers/HomeController.java
+++ src/main/java/dev/cosmingherghe/pma/controllers/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,7 +32,14 @@ public class HomeController {
     EmployeeService employeeService;
 
     @RequestMapping("/")
-    public String displayHome(Model model) throws JsonProcessingException {
+    public String displayHome(Model model, Principal principal) throws JsonProcessingException {
+
+        String loggedInUser = "null";
+
+        if(principal != null)
+            loggedInUser = principal.getName();
+
+        model.addAttribute("loggedInUser", loggedInUser);
 
         model.addAttribute("projVersionNumber", projVersion);
 

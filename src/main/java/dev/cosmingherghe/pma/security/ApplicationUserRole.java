070ae1d235e51e7b3503fcca22205734570458bf
Cosmin Gherghe <cosminlinux@gmail.com>
2020-12-08 02:41:39 +0000

    add Authorities to ROLES

--- src/main/java/dev/cosmingherghe/pma/security/ApplicationUserRole.java
+++ src/main/java/dev/cosmingherghe/pma/security/ApplicationUserRole.java
@@ -1,6 +1,22 @@
 package dev.cosmingherghe.pma.security;
 
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static dev.cosmingherghe.pma.security.AplicationUserAuthority.*;
+
 public enum ApplicationUserRole {
-    ADMIN,
-    USER
+    ADMIN(Sets.newHashSet(PROJECT_READ, PROJECT_WRITE, USER_READ, USER_WRITE)),
+    USER(Sets.newHashSet());
+
+    private final Set<AplicationUserAuthority> authorities;
+
+    ApplicationUserRole(Set<AplicationUserAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    public Set<AplicationUserAuthority> getAuthorities() {
+        return authorities;
+    }
 }

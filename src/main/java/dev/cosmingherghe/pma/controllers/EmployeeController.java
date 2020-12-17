ef3b9df8aaaced72cd30080bd2beb2843a304d78
Cosmin Gherghe <cosminlinux@gmail.com>
2020-12-17 23:50:05 +0000

    add Error Model for saving employee

--- src/main/java/dev/cosmingherghe/pma/controllers/EmployeeController.java
+++ src/main/java/dev/cosmingherghe/pma/controllers/EmployeeController.java
@@ -7,8 +7,10 @@ import dev.cosmingherghe.pma.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -43,7 +45,10 @@ public class EmployeeController {
     }
 
     @PostMapping("/save")
-    public String saveEmployee(Employee employee, Model model) {
+    public String saveEmployee(@Valid Employee employee, Model model, Errors errors) {
+
+        if(errors.hasErrors())
+            return "/employees/new-employee";
 
         employeeService.save(employee);
 

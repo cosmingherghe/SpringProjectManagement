000c08ea90cef0072934d10120da7c41ed097a5e
Cosmin Gherghe <cosminlinux@gmail.com>
2020-12-17 23:51:31 +0000

    Employee name should not be blank

--- src/main/java/dev/cosmingherghe/pma/entities/Employee.java
+++ src/main/java/dev/cosmingherghe/pma/entities/Employee.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
@@ -17,7 +18,7 @@ public class Employee {
     @SequenceGenerator(name = "employee_generator", sequenceName = "employee_seq", allocationSize = 1)
     private Long employeeId;
 
-    @NotNull
+    @NotBlank
     @Size(min=2, max = 50)
     private String firstName;
 

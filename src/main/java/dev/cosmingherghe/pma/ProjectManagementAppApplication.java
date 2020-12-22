3399ff7247c6ef1c3fd090d111a27ece00e89cdb
Cosmin Gherghe <cosminlinux@gmail.com>
2020-12-22 20:14:56 +0000

    Enabling HTTPS/SSL in Spring Boot

--- src/main/java/dev/cosmingherghe/pma/ProjectManagementAppApplication.java
+++ src/main/java/dev/cosmingherghe/pma/ProjectManagementAppApplication.java
@@ -1,25 +1,69 @@
 package dev.cosmingherghe.pma;
 
-import dev.cosmingherghe.pma.dao.ProjectRepository;
 import dev.cosmingherghe.pma.dao.EmployeeRepository;
+import dev.cosmingherghe.pma.dao.ProjectRepository;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjectManagementAppApplication {
 
-	@Autowired
-	ProjectRepository projectRepository;
+    @Autowired
+    ProjectRepository projectRepository;
 
-	@Autowired
+    @Autowired
     EmployeeRepository employeeRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectManagementAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementAppApplication.class, args);
+    }
 
-	//  seeding the database with some pre-existing data
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        // Enable SSL Trafic
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        // Add HTTP to HTTPS redirect
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+    }
+
+    /*
+    We need to redirect from HTTP to HTTPS. Without SSL, this application used
+    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
+    redirected to HTTPS on 8443.
+     */
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8082);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+
+
+//  seeding the database with some pre-existing data
 //	@Bean
 //	CommandLineRunner runner() {
 //
